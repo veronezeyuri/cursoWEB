df8386f508121a7842ca2a5031067384991d5129
yuli <yuriveroneze6@gmail.com>
2022-10-24 17:31:02 -0300

    24/10

---
+++ Dashboard/orders.js
@@ -0,0 +1,38 @@
+const Orders = [
+  {
+    productName: "Foldable Mini Drone",
+    productNumber: "85631",
+    paymentStatus: "Vencido",
+    shipping: "Pendente",
+  },
+  {
+    productName: "LARVENDER KF102 Drone",
+    productNumber: "36378",
+    paymentStatus: "Reembolsado",
+    shipping: "Negado",
+  },
+  {
+    productName: "Ruko F11 Pro Drone",
+    productNumber: "49347",
+    paymentStatus: "Vencido",
+    shipping: "Negado",
+  },
+  {
+    productName: "Drone with Camera Drone",
+    productNumber: "96996",
+    paymentStatus: "Pago",
+    shipping: "Entregue",
+  },
+  {
+    productName: "GPS 4K Drone",
+    productNumber: "22821",
+    paymentStatus: "Pago",
+    shipping: "Entregue",
+  },
+  {
+    productName: "Yuri",
+    productNumber: "2222",
+    paymentStatus: "Muito caro",
+    shipping: "Sem devolução",
+  },
+];
